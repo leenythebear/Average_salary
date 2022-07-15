76430fdcd5e7da83abe2169fec34f84665d5d353
leenythebear <Leeny@inbox.ru>
2022-07-15 11:48:54 +0700

    add function to calculate average salary

--- headhunter.py
+++ headhunter.py
@@ -55,3 +55,19 @@ def get_all_vacancies(language):
     #     },
     # )
 
+
+def predict_rub_salary_hh(vacancy):
+    salary = vacancy["salary"]
+    if not salary:
+        average_salary = None
+    elif not salary["currency"]:
+        average_salary = None
+    elif salary["currency"] != "RUR":
+        average_salary = None
+    elif not salary["to"]:
+        average_salary = salary["from"] * 1.2
+    elif not salary["from"]:
+        average_salary = salary["to"] * 0.8
+    else:
+        average_salary = (salary["from"] + salary["to"]) / 2
+    return average_salary
