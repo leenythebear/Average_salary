a9a51ff4027242f059f83214ae961bf2f70b76ba
leenythebear <Leeny@inbox.ru>
2022-07-17 21:07:39 +0700

    add function to get response

--- superjob.py
+++ superjob.py
@@ -14,12 +14,10 @@ languages = ['Python', 'JavaScript', 'Java', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'G
 sj_url = 'https://api.superjob.ru/2.0/vacancies'
 
 
-def get_vacancies_sj(language):
+def get_response(language, page, url):
     headers = {'X-Api-App-Id': secret_key}
-    params = {'key': 48, 'keywords': f'Разработчик {language}', 'town': 4}
-    page = 0
-    pages_number = 1
-    response = requests.get(sj_url, headers=headers, params=params)
+    params = {'key': 48, 'keywords': f'Разработчик {language}', 'town': 4, 'page': page}
+    response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
     return response.json()
 
